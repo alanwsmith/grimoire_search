62b6070ce64ae46dd021383c1bd520e7fa22ed28
Alan Smith <commit@alanwsmith.com>
2023-03-03 22:09:58 -0500

    Cleanup

--- src/page.rs
+++ src/page.rs
@@ -1,5 +1,3 @@
-#![allow(non_snake_case)]
-#![allow(dead_code)]
 use serde::{
     Deserialize,
     Serialize,
--- src/search.rs
+++ src/search.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)]
-#![allow(unreachable_code)]
-#![allow(non_snake_case)]
 use futures::executor::block_on;
 use meilisearch_sdk::client::*;
 
@@ -33,12 +30,9 @@ impl Search {
         let ms_key = get_credentials(
             env_key, cred_key, cred_user,
         );
-
         let client =
             Client::new(ms_url, ms_key.unwrap());
-
         if self.params.len() == 2 {
-            // let holder: Vec<String> = vec![];
             block_on(async move {
                 let my_stuff = client
                     .index("grimoire")
@@ -51,63 +45,14 @@ impl Search {
                     .await
                     .unwrap()
                     .hits;
-                //dbg!(my_stuff);
-
-                // self.search_engine_response =
-                // let tmpthing: Vec<String> =
                 self.search_engine_response = Some(
                     my_stuff
                         .iter()
                         .map(|ya| ya.result.clone())
                         .collect(),
                 );
-
-                // for thing in my_stuff {
-                //     dbg!(thing.result.fileName);
-                //     holder.push(thing.result.fileName);
-                // }
-
-                // if let Ok(thing) = &my_stuff {
-                //     dbg!(thing);
-                //     //     //     println!(
-                //     //     //         "{}",
-                //     //     //         thing
-                //     //     //             .result
-                //     //     //             .fileName
-                //     //     //             .unwrap()
-                //     //     //     );
-                // }
-                // // dbg!("HEREREREERERERER");
-                // self.search_engine_response =
-                //     Some(vec![]);
             });
         }
-
-        // if self.params.len() == 1 {
-        //     println!("Ready...")
-        // }
-        // else if self.params.len() == 2 {
-        //     block_on(async move {
-        //         let my_stuff = client
-        //             .index("grimoire")
-        //             .search()
-        //             .with_limit(14)
-        //             .with_query(
-        //                 &self.params[1].as_str(),
-        //             )
-        //             .execute::<Page>()
-        //             .await;
-        //         // for thing in my_stuff {
-        //         //     println!(
-        //         //         "{}",
-        //         //         thing
-        //         //             .result
-        //         //             .fileName
-        //         //             .unwrap()
-        //         //     );
-        //         // }
-        //     })
-        // }
     }
 }
 
